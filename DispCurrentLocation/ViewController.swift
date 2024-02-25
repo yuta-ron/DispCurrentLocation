351a4198dba68af50d58795efd3916799ffa8226
yuta-ron <jan.joqr@gmail.com>
2024-02-25 13:53:19 +0900

    Initial Commit

---
+++ DispCurrentLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DispCurrentLocation
+//
+//  Created by yutaron on 2024/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
