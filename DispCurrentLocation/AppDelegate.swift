237f38b596bf11eae016d15fc310811f4e55c68e
yuta-ron <jan.joqr@gmail.com>
2024-02-29 23:59:40 +0900

    refactoring

--- DispCurrentLocation/AppDelegate.swift
+++ DispCurrentLocation/AppDelegate.swift
@@ -13,7 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, _) in
+            if granted {
+                UNUserNotificationCenter.current().delegate = self
+            }
+        }
+        
         return true
     }
     
@@ -29,15 +34,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-            if #available(iOS 14.0, *) {
-                completionHandler([[.banner, .list, .sound]])
-            } else {
-                completionHandler([[.alert, .sound]])
-            }
     }
 }
--- DispCurrentLocation/ViewController.swift
+++ DispCurrentLocation/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         placeLabel.text = "Loading..."
         placeLabel.textAlignment = .center
         placeLabel.font = UIFont.systemFont(ofSize: 28)
-
+        
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         
@@ -35,24 +35,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // アプリ使用中の位置情報の許可をユーザに求める
         locationManager.startUpdatingLocation() // 位置情報の取得を開始
         
-        
-        
-        // Notification
-        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
-        UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert, .sound]) { (granted, error) in
-            guard granted else {
-                return
-            }
-            let content = UNMutableNotificationContent()
-            content.title = "Test"
-            content.body = "Notification"
-            content.sound = .none
-            let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: TimeInterval(3), repeats: false)
-            let request = UNNotificationRequest.init(identifier: "identifier-1", content: content, trigger: trigger)
-            UNUserNotificationCenter.current().add(request)
-            print("add")
-        }
-        //
+        // 通知音声は専用ディレクトリ管理なので
+        // お掃除
+//        disposeSoundFiles()
+//        
+//        do {
+//            try FileManager.default.createDirectory(at: getNotificationSoundDirUrl(), withIntermediateDirectories: true, attributes: nil)
+//            print("ディレクトリ作成に成功しました")
+//        } catch {
+//            print("ディレクトリ作成に失敗しました")
+//        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -69,6 +61,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 locationStr.append(place.locality!)
                 locationStr.append(place.thoroughfare!)
                 
+                print(place)
+                
                 self.placeLabel.text = locationStr
             }
         }
@@ -81,44 +75,114 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func buttonTapped(_ sender: Any) {
-        notifyByZundamon(prefecture: "nagasaki")
+        notifyByZundamon(prefectureName: "長崎県")
     }
     
-    
-    // Todo: リアルタイム通知的なことをやりたい
-    func notifyByZundamon(prefecture: String) {
-        if let soundURL = Bundle.main.url(forResource: prefecture, withExtension: "wav", subdirectory: "zundamon") {
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-                audioPlayer?.prepareToPlay()
-                audioPlayer?.play()
-            } catch {
-                print("音声ファイルの読み込みに失敗しました。", error)
-            }
-        }
+    func notifyByZundamon(prefectureName: String) {
+        // フォアグラウンドだったら
+//        guard let soundURL = getSoundUrl(prefecture: prefecture) else {
+//            print("音声ファイルの読み込みに失敗しました。")
+//            return;
+//        }
+//        
+//        do {
+//            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+//            audioPlayer?.prepareToPlay()
+//            audioPlayer?.play()
+//        } catch {
+//            print("音声ファイルの再生に失敗しました", error)
+//        }
         
-        sendNotification()
+        // バックグラウンドだったら
+        sendNotification(prefectureName: prefectureName)
     }
     
-    func sendNotification() {
+    func sendNotification(prefectureName: String) {
         let content = UNMutableNotificationContent()
-        content.title = NSString.localizedUserNotificationString(forKey: "タイトル", arguments: nil)
-        content.body = NSString.localizedUserNotificationString(forKey: "メッセージ内容", arguments: nil)
-        // content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: soundName))
+        content.title = NSString.localizedUserNotificationString(forKey: "\(prefectureName)に入りました", arguments: nil)
+//        content.body = NSString.localizedUserNotificationString(forKey: "メッセージ内容", arguments: nil)
         
+        let alphaName = prefectureNameToAlpha(prefectureName: prefectureName)
+        if (alphaName != nil) {
+            print(alphaName!)
+            content.sound = UNNotificationSound(named: UNNotificationSoundName("\(alphaName!).mp3"))
+        } else {
+            content.sound = .default
+        }
+
         // トリガーを設定（ここでは5秒後に設定）
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
 
         // リクエストを作成
-        let request = UNNotificationRequest(identifier: "CustomSoundNotification", content: content, trigger: trigger)
-
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         // リクエストを通知センターに追加
-        let center = UNUserNotificationCenter.current()
-        center.add(request) { (error : Error?) in
+        UNUserNotificationCenter.current().add(request) { (error : Error?) in
             if let theError = error {
                 print(theError.localizedDescription)
             }
         }
+    }
+    
+    func getSoundUrl(prefectureName: String) -> URL? {
+        guard let alphaName = prefectureNameToAlpha(prefectureName: prefectureName) else {
+            return nil
+        }
+        
+        return Bundle.main.url(forResource: alphaName, withExtension: "mp3", subdirectory: "zundamon")
+    }
+    
+    func prefectureNameToAlpha(prefectureName: String) -> String? {
+        let codes = [
+                "北海道": "hokkaido",
+                "青森県": "aomori",
+                "岩手県": "iwate",
+                "宮城県": "miyagi",
+                "秋田県": "akita",
+                "山形県": "yamagata",
+                "福島県": "fukushima",
+                "茨城県": "ibaraki",
+                "栃木県": "tochigi",
+                "群馬県": "gunma",
+                "埼玉県": "saitama",
+                "千葉県": "chiba",
+                "東京都": "tokyo",
+                "神奈川県": "kanagawa",
+                "新潟県": "niigata",
+                "富山県": "toyama",
+                "石川県": "ishikawa",
+                "福井県": "fukui",
+                "山梨県": "yamanashi",
+                "長野県": "nagano",
+                "岐阜県": "gifu",
+                "静岡県": "shizuoka",
+                "愛知県": "aichi",
+                "三重県": "mie",
+                "滋賀県": "shiga",
+                "京都府": "kyoto",
+                "大阪府": "osaka",
+                "兵庫県": "hyogo",
+                "奈良県": "nara",
+                "和歌山県": "wakayama",
+                "鳥取県": "tottori",
+                "島根県": "shimane",
+                "岡山県": "okayama",
+                "広島県": "hiroshima",
+                "山口県": "yamaguchi",
+                "徳島県": "tokushima",
+                "香川県": "kagawa",
+                "愛媛県": "ehime",
+                "高知県": "kochi",
+                "福岡県": "fukuoka",
+                "佐賀県": "saga",
+                "長崎県": "nagasaki",
+                "熊本県": "kumamoto",
+                "大分県": "oita",
+                "宮崎県": "miyazaki",
+                "鹿児島県": "kagoshima",
+                "沖縄県": "okinawa"
+            ]
+
+            return codes.first(where: { $0.key == prefectureName })?.value
     }
 }
 
